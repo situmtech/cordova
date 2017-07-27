a1c58aaa80ec564e2adcbabdf6b7332af7d87006
Alberto Doval <alberto.doval@cocodin.com>
2017-07-28 00:37:20 +0200

    Added some refactorings to make easier the project bootstrapping

---
+++ www/android/situm.js
@@ -0,0 +1,22 @@
+var exec = require('cordova/exec');
+
+var PLUGIN_NAME = 'Situm';
+
+var Situm = {
+  setApiKey: function(email, apiKey, cb) {
+    exec(cb, null, PLUGIN_NAME, 'setApiKey', [email, apiKey]);
+  },
+  setUserPass: function(email, apiKey, cb) {
+    exec(cb, null, PLUGIN_NAME, 'setUserPass', [email, password]);
+  },
+  fetchBuildings: function(cb) {
+    exec(cb, null, PLUGIN_NAME, 'fetchBuildings', []);
+  },
+  startPositioning: function(arrBuilding,cb) {
+    exec(cb, null, PLUGIN_NAME, 'startPositioning', arrBuilding);
+  },
+  stopPositioning: function(cb) {
+    exec(cb, null, PLUGIN_NAME, 'stopPositioning', []);
+  }
+};
+module.exports = Situm;
