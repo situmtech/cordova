c1bfa3490ed3af0295a01d0d00bdf34ac29b9dcd
Oscar Fuentes <oscar.fuentes@cocodin.com>
2017-08-03 12:09:33 +0200

    Added functions calls to situm.js

--- www/android/situm.js
+++ www/android/situm.js
@@ -9,23 +9,38 @@ var Situm = {
   setUserPass: function (email, apiKey, cb) {
     exec(cb, null, PLUGIN_NAME, 'setUserPass', [email, password]);
   },
-  fetchBuildings: function (cb) {
-    exec(cb, null, PLUGIN_NAME, 'fetchBuildings', []);
-  },
   startPositioning: function (arrBuilding, cb) {
     exec(cb, null, PLUGIN_NAME, 'startPositioning', arrBuilding);
   },
   stopPositioning: function (cb) {
     exec(cb, null, PLUGIN_NAME, 'stopPositioning', []);
   },
+  fetchBuildings: function (cb) {
+    exec(cb, null, PLUGIN_NAME, 'fetchBuildings', []);
+  },
   fetchFloorsFromBuilding: function (building, cb) {
     exec(cb, null, PLUGIN_NAME, 'fetchFloorsFromBuilding', [building]);
+  },
+  fetchIndoorPOIsFromBuilding: function (building, cb) {
+    exec(cb, null, PLUGIN_NAME, 'fetchIndoorPOIsFromBuilding', [building]);
+  },
+  fetchOutdoorPOIsFromBuilding: function (building, cb) {
+    exec(cb, null, PLUGIN_NAME, 'fetchOutdoorPOIsFromBuilding', [building]);
+  },
+  fetchEventsFromBuilding: function (building, cb) {
+    exec(cb, null, PLUGIN_NAME, 'fetchEventsFromBuilding', [building]);
   },
   fetchPoiCategories: function (cb) {
     exec(cb, null, PLUGIN_NAME, 'fetchPoiCategories');
   },
   fetchMapFromFloor: function (floor, cb) {
     exec(cb, null, PLUGIN_NAME, 'fetchMapFromFloor', [floor]);
+  },
+  fetchPoiCategoryIconNormal: function (category, cb) {
+    exec(cb, null, PLUGIN_NAME, 'fetchPoiCategoryIconNormal', [category]);
+  },
+  fetchPoiCategoryIconSelected: function (category, cb) {
+    exec(cb, null, PLUGIN_NAME, 'fetchPoiCategoryIconSelected', [category]);
   }
 };
 module.exports = Situm;
