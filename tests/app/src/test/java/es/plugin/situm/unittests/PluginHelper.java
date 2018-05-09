f9cc9dce1036447b2f50107b0b3339a005f36e1e
Oscar Fuentes <oscar.fuentes@cocodin.com>
2018-05-09 17:16:14 +0200

    Add libs to read jsons and config init method

---
+++ tests/app/src/test/java/es/plugin/situm/unittests/PluginHelper.java
@@ -0,0 +1,49 @@
+/*
+       Licensed to the Apache Software Foundation (ASF) under one
+       or more contributor license agreements.  See the NOTICE file
+       distributed with this work for additional information
+       regarding copyright ownership.  The ASF licenses this file
+       to you under the Apache License, Version 2.0 (the
+       "License"); you may not use this file except in compliance
+       with the License.  You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License.
+*/
+
+package es.plugin.situm.unittests;
+
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileReader;
+import java.net.URL;
+
+public class PluginHelper{
+
+
+  JSONParser parser;
+  ClassLoader classLoader;
+  @Before
+  public void prepareTests() {
+    parser = new JSONParser();
+    classLoader = getClass().getClassLoader();
+  }
+
+  @Test
+  public void fetchBuildings() throws Exception {
+      URL resource = classLoader.getResource("fetch_buildings.json");
+      File fetchBuildings = new File(resource.getFile());
+      Object fileContent = parser.parse(new FileReader(fetchBuildings));
+      JSONArray buildings = (JSONArray) fileContent;
+  }
+}
