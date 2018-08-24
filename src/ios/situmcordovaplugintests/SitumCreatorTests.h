921b8e65300111da1cbbbc5e45c324fff6546e45
Alberto Doval <alberto.doval@cocodin.com>
2018-08-24 12:52:37 +0200

    Added creators for: -angle -bounds -cartesianCoordinates -coordinates -dimensions -point

--- src/ios/situmcordovaplugintests/SitumCreatorTests.h
+++ src/ios/situmcordovaplugintests/SitumCreatorTests.h
@@ -10,6 +10,19 @@
 
 @interface SitumCreatorTests : NSObject
 
+//angle
++ (SITAngle *) createAngleFromDegrees; //angle1.json
++ (SITAngle *) createAngleFromRadians; //angle2.json
+//bounds
++ (SITBounds) createBoundsWithArray; //bounds1.json
++ (SITBounds) createBounds; //bounds2.json
+//cartesianCoordinate
++ (SITCartesianCoordinate *) createCartesianCoordinate; //cartesianCoordinate1.json
+//coordinate
++ (CLLocationCoordinate2D) createCoordinate; //coordinate1.json
+//dimensions
++ (SITDimensions *) createDimensions; //dimensions1.json
+//location
 + (SITLocation *) createLocationWithBuildingFloorAndCartesianCoordinates; //location1.json
 + (SITLocation *) createLocationWithCoordinate; //location2.json
 + (SITLocation *) createLocationWithBuildingAndCoordinate; //location3.json
@@ -20,5 +33,14 @@
 + (SITLocation *) indoorLocationWithIndoorBearingQualityLow; //location8.json
 + (SITLocation *) indoorLocationWithIndoorBearingQualityHigh; //location9.json
 + (SITLocation *) outdoorLocation; //location10.json
+//point
++ (SITPoint *) createPointWithCoordinate; //point1.json
++ (SITPoint *) createPointWithCoordinateAndBuildingId; //point2.json
++ (SITPoint *) createPointWithBuildingIdAndFloor; //point3.json
++ (SITPoint *) createPointWithBuildingWithAngleFromRadians; //point4.json
++ (SITPoint *) createPointWithBuildingWithAngleFromDegrees; //point5.json
++ (SITPoint *) createPointWithBuildingWithAddress; //point6.json
++ (SITPoint *) createPointWithBuildingWithInfo; //point7.json
++ (SITPoint *) createPointWithBuildingWithPicture; //point8.json
 
 @end
--- src/ios/situmcordovaplugintests/SitumCreatorTests.m
+++ src/ios/situmcordovaplugintests/SitumCreatorTests.m
@@ -10,6 +10,57 @@
 
 @implementation SitumCreatorTests
 
+//angle1.json
++ (SITAngle *) createAngleFromDegrees {
+    SITAngle *angle = [[SITAngle alloc] initWithDegrees:47];
+    return angle;
+}
+
+//angle2.json
++ (SITAngle *) createAngleFromRadians {
+    SITAngle *angle = [[SITAngle alloc] initWithRadians:1.4];
+    return angle;
+}
+
+//bounds1.json
++ (SITBounds) createBoundsWithArray {
+    SITBounds Bounds;
+    Bounds.southWest = CLLocationCoordinate2DMake(2, 4);
+    Bounds.southEast = CLLocationCoordinate2DMake(6, 3);
+    Bounds.northWest = CLLocationCoordinate2DMake(2, 6);
+    Bounds.northEast = CLLocationCoordinate2DMake(5, 8);
+    return Bounds;
+}
+
+//bounds2.json
+//NOTE: it is redundant in ios, it is equals to previous bounds1.json
++ (SITBounds) createBounds {
+    SITBounds Bounds;
+    Bounds.southWest = CLLocationCoordinate2DMake(2, 4);
+    Bounds.southEast = CLLocationCoordinate2DMake(6, 3);
+    Bounds.northWest = CLLocationCoordinate2DMake(2, 6);
+    Bounds.northEast = CLLocationCoordinate2DMake(5, 8);
+    return Bounds;
+}
+
+//cartesianCoordinate1.json
++ (SITCartesianCoordinate *) createCartesianCoordinate {
+    SITCartesianCoordinate *cartesianCoordinate = [[SITCartesianCoordinate alloc] initWithX:5 y:7];
+    return cartesianCoordinate;
+}
+
+//coordinate1.json
++ (CLLocationCoordinate2D) createCoordinate {
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(23, 43);
+    return coordinate;
+}
+
+//dimensions1.json
++ (SITDimensions *) createDimensions {
+    SITDimensions *dimensions = [[SITDimensions alloc] initWithWidth:5 height:7];
+    return dimensions;
+}
+
 //location1.json
 + (SITLocation *) createLocationWithBuildingFloorAndCartesianCoordinates {
     NSTimeInterval timestamp = 14676784;
@@ -159,6 +210,69 @@
     float accuracy = 5;
     SITLocation *location = [[SITLocation alloc] initWithTimestamp:timestamp position:position bearing: bearing cartesianBearing:cartesianBearing quality:quality accuracy:accuracy provider:provider];
     return location;
+}
+
+//point1.json
++ (SITPoint *) createPointWithCoordinate {
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(3, 6);
+    SITPoint *point = [[SITPoint alloc] initWihtCoordinate:coordinate];
+    return point;
+}
+
+//point2.json
++ (SITPoint *) createPointWithCoordinateAndBuildingId {
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(54, 64);
+    SITCartesianCoordinate *cartesianCoordinate = [[SITCartesianCoordinate alloc] initWithX:0 y:0];
+    SITPoint *point  = [[SITPoint alloc]  initWithCoordinate:coordinate buildingIdentifier:@"101" floorIdentifier:@"-1" cartesianCoordinate: cartesianCoordinate];
+    return point;
+}
+
+//point3.json
++ (SITPoint *) createPointWithBuildingIdAndFloor {
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(0, 0);
+    SITCartesianCoordinate *cartesianCoordinate = [[SITCartesianCoordinate alloc] initWithX:5 y:7];
+    SITPoint *point  = [[SITPoint alloc]  initWithCoordinate:coordinate buildingIdentifier:@"101" floorIdentifier:@"12" cartesianCoordinate: cartesianCoordinate];
+    return point;
+}
+
+//point4.json
++ (SITPoint *) createPointWithBuildingWithAngleFromDegrees {
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(12.3,14.0);
+    SITCartesianCoordinate *cartesianCoordinate = [[SITCartesianCoordinate alloc] initWithX:4 y:7];
+    SITPoint *point  = [[SITPoint alloc]  initWithCoordinate:coordinate buildingIdentifier:@"-1" floorIdentifier:@"12" cartesianCoordinate: cartesianCoordinate];
+    return point;
+}
+
+//point5.json
++ (SITPoint *) createPointWithBuildingWithAngleFromRadians {
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(12.3,14.0);
+    SITCartesianCoordinate *cartesianCoordinate = [[SITCartesianCoordinate alloc] initWithX:4 y:7];
+    SITPoint *point  = [[SITPoint alloc]  initWithCoordinate:coordinate buildingIdentifier:@"-1" floorIdentifier:@"12" cartesianCoordinate: cartesianCoordinate];
+    return point;
+}
+
+//point6.json
++ (SITPoint *) createPointWithBuildingWithAddress {
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(12.3,14.0);
+    SITCartesianCoordinate *cartesianCoordinate = [[SITCartesianCoordinate alloc] initWithX:4 y:7];
+    SITPoint *point  = [[SITPoint alloc]  initWithCoordinate:coordinate buildingIdentifier:@"-1" floorIdentifier:@"12" cartesianCoordinate: cartesianCoordinate];
+    return point;
+}
+
+//point7.json
++ (SITPoint *) createPointWithBuildingWithInfo {
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(12.3,14.0);
+    SITCartesianCoordinate *cartesianCoordinate = [[SITCartesianCoordinate alloc] initWithX:4 y:7];
+    SITPoint *point  = [[SITPoint alloc]  initWithCoordinate:coordinate buildingIdentifier:@"-1" floorIdentifier:@"12" cartesianCoordinate: cartesianCoordinate];
+    return point;
+}
+
+//point8.json
++ (SITPoint *) createPointWithBuildingWithPicture {
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(12.3,14.0);
+    SITCartesianCoordinate *cartesianCoordinate = [[SITCartesianCoordinate alloc] initWithX:4 y:7];
+    SITPoint *point  = [[SITPoint alloc]  initWithCoordinate:coordinate buildingIdentifier:@"-1" floorIdentifier:@"12" cartesianCoordinate: cartesianCoordinate];
+    return point;
 }
 
 @end
