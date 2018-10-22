9d311f16acbeb6efc715b5c66db89748bc7bf177
Cristina S. Barreiro <cristina.sbarreiro@situm.es>
2018-10-22 13:24:18 +0200

    Remove unused methods

--- src/ios/situmcordovaplugintests/InputTests/LocationRequestTest.m
+++ src/ios/situmcordovaplugintests/InputTests/LocationRequestTest.m
@@ -25,15 +25,6 @@
     _helper = [TestingHelper new];
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
 - (void) testLegacyLocationRequest {
     // Create the object to convert and test
     NSString *filename =  @"locationRequest1";
