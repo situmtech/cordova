33dce8f145f6566aee08103816a3bc6927011004
Cristina S. Barreiro <cristina.sbarreiro@situm.es>
2018-12-03 14:01:49 +0100

    Fix error when creating test data

--- src/ios/situmcordovaplugintests/SitumCreatorTests.m
+++ src/ios/situmcordovaplugintests/SitumCreatorTests.m
@@ -184,8 +184,6 @@
 //locationRequest4.json
 + (SITLocationRequest *) createLocationRequestWithBatterySaver {
     SITLocationRequest *locationRequest = [[SITLocationRequest alloc] initWithBuildingId:@"1051"];
-    [locationRequest setUseDeadReckoning:true];
-    [locationRequest setUseGps:true];
     [locationRequest setUpdateInterval: 1800];
     return locationRequest;
 }
