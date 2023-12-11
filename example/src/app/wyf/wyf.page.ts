f066cd8bbee43f66ea21bca07320aee4ff35d7f7
Cristian Mayán <cristian.mayan@situm.com>
2023-12-11 19:11:55 +0100

    Centralized all credentials in constants.ts

--- example/src/app/wyf/wyf.page.ts
+++ example/src/app/wyf/wyf.page.ts
@@ -1,10 +1,17 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import {
+  CUSTOM_ELEMENTS_SCHEMA,
+  Component,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 import {
   IonHeader,
   IonToolbar,
   IonTitle,
   IonContent,
 } from '@ionic/angular/standalone';
+
+import * as Constants from 'src/constants';
 
 @Component({
   selector: 'app-wyf',
@@ -16,5 +23,23 @@ import {
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class WYFPage {
-  constructor() {}
+  @ViewChild('mapView') mapView: ElementRef | undefined;
+
+  ngAfterViewInit() {
+    // Set the credentials in the HTMLElement
+    this.mapView?.nativeElement.setAttribute(
+      'viewer-domain',
+      Constants.VIEWER_DOMAIN
+    );
+
+    this.mapView?.nativeElement.setAttribute(
+      'situm-api-key',
+      Constants.API_KEY
+    );
+
+    this.mapView?.nativeElement.setAttribute(
+      'building-identifier',
+      Constants.BUILDING_IDENTIFIER
+    );
+  }
 }
