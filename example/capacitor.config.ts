ee147ed271ee4bf294c3996cb94ee12589df1565
Óscar Iglesias <oscar.iglesias@situm.com>
2024-02-08 10:39:06 +0100

    SIS-722: Pipelines commit tests

--- example/capacitor.config.ts
+++ example/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.situm.capacitor_example',
+  appId: 'com.situm.capacitorexample',
   appName: 'Situm Capacitor Example',
   webDir: 'www',
   server: {
